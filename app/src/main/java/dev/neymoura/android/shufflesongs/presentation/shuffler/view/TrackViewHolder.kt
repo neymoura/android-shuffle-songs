01f87c873ab9b1909a735fee7b2d9082d640a9d5
Ney Moura <neymourac@gmail.com>
2019-11-10 18:02:51 -0300

    Track ViewHolder is getting cooler!

--- app/src/main/java/dev/neymoura/android/shufflesongs/presentation/shuffler/view/TrackViewHolder.kt
+++ app/src/main/java/dev/neymoura/android/shufflesongs/presentation/shuffler/view/TrackViewHolder.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import dev.neymoura.android.shufflesongs.R
+import dev.neymoura.android.shufflesongs.utils.CircleTransform
 import dev.neymoura.android.songsprovider.model.MusicalData
 import kotlinx.android.synthetic.main.view_holder_track.view.*
 
@@ -24,7 +26,13 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(musicalData: MusicalData) {
         with(itemView) {
-            trackName.text = musicalData.artistName
+            Picasso.get().load(musicalData.artworkUrl).transform(CircleTransform()).into(albumImage)
+            trackTitle.text = musicalData.trackName
+            trackSubtitle.text = resources.getString(
+                R.string.track_subtitle,
+                musicalData.artistName,
+                musicalData.primaryGenreName
+            )
         }
     }
 
